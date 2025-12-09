d2d6c70eddb47db6ca5112b087d70e33ab6c4efb
Lance Kian Flores <lancekian12@gmail.com>
2025-12-09 10:02:39 +0800

    finished project setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Page = () => {
     return (
-        <h1 className="text-center">Welcome to NextJS 16</h1>
+        <section>
+            <h1 className="text-center">The Hub for Every Dev <br/> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
+        </section>
     )
 }
 export default Page
